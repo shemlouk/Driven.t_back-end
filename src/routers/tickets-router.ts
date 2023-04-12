7f173095d333482bd19234158b9a9fdd4ea32f71
samuel <shemlouk@gmail.com>
2023-04-11 21:54:51 -0300

    feat(routers): create tickets router and add to app

---
+++ src/routers/tickets-router.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { authenticateToken } from '@/middlewares';
+
+const ticketsRouter = Router();
+
+ticketsRouter.all('/*', authenticateToken);
+
+export { ticketsRouter };
