0c311ef600716faa48814c4b8ba1d932c5080faf
samuel <shemlouk@gmail.com>
2023-04-14 21:13:33 -0300

    fix: change send to sendStatus on enrollment controller

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -35,6 +35,6 @@ export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response
     const address = await enrollmentsService.getAddressFromCEP(cep);
     res.status(httpStatus.OK).send(address);
   } catch (error) {
-    return res.send(httpStatus.NO_CONTENT);
+    return res.sendStatus(httpStatus.NO_CONTENT);
   }
 }
