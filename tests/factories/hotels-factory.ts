c8beba65d4230d71c9f80168c75e780267c1aea0
samuel <shemlouk@gmail.com>
2023-04-21 17:32:44 -0300

    test: add create hotel and room factories

---
+++ tests/factories/hotels-factory.ts
@@ -0,0 +1,13 @@
+import { Hotel, Room } from '@prisma/client';
+import faker from '@faker-js/faker';
+import { prisma } from '@/config';
+
+export const createHotel = async (params: Partial<Hotel> = {}): Promise<Hotel> => {
+  const { name = faker.company.companyName(), image = faker.image.imageUrl() } = params;
+  return prisma.hotel.create({ data: { name, image } });
+};
+
+export const createRoom = async (hotelId: number, params: Partial<Room> = {}): Promise<Room> => {
+  const { name = faker.lorem.word(), capacity = Number(faker.random.numeric(1, { bannedDigits: ['0'] })) } = params;
+  return prisma.room.create({ data: { name, capacity, hotelId } });
+};
