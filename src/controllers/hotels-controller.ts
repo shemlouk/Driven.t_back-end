60e281f5549ba8a4a8dfdc62cd34bb25e870b1c9
samuel <shemlouk@gmail.com>
2023-04-21 18:53:26 -0300

    feat: add /hotels/:hotelId GET endpoint

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -6,7 +6,19 @@ import { AuthenticatedRequest } from '@/middlewares';
 export const getHotels = async (_req: AuthenticatedRequest, res: Response) => {
   try {
     const hotels = await hotelsService.getHotels();
+    if (!hotels.length) return res.sendStatus(httpStatus.NOT_FOUND);
     return res.send(hotels);
+  } catch (error) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+};
+
+export const getRoomsFromHotel = async (req: AuthenticatedRequest, res: Response) => {
+  const { hotelId } = req.params;
+  try {
+    const hotelWithRooms = await hotelsService.getHotelWithRooms(+hotelId);
+    if (!hotelWithRooms) return res.sendStatus(httpStatus.NOT_FOUND);
+    return res.send(hotelWithRooms);
   } catch (error) {
     return res.sendStatus(httpStatus.BAD_REQUEST);
   }
