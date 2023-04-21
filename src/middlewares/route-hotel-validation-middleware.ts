98b212093b83cd94abb6b514aec0beb2520d82f5
samuel <shemlouk@gmail.com>
2023-04-21 14:18:28 -0300

    feat: create validation middleware for hotels route

---
+++ src/middlewares/route-hotel-validation-middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Response } from 'express';
+import httpStatus from 'http-status';
+import { AuthenticatedRequest } from './authentication-middleware';
+import ticketsService from '@/services/tickets-service';
+
+const routeHotelValidationMiddleware = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  const { userId } = req;
+
+  try {
+    const [userTicket] = await ticketsService.getAllTicketsFromUserId(userId);
+
+    if (!userTicket) return res.sendStatus(httpStatus.NOT_FOUND);
+
+    const isNotPaid = userTicket.status !== 'PAID';
+    const isRemote = userTicket.TicketType.isRemote;
+    const doesNotIncludesHotel = !userTicket.TicketType.includesHotel;
+
+    if (isNotPaid || isRemote || doesNotIncludesHotel) return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default routeHotelValidationMiddleware;
