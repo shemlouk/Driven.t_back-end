57410ba720fc03dc5a96ec641ec8dfabc83cd907
samuel <shemlouk@gmail.com>
2023-04-25 19:48:17 -0300

    test: create GET /booking test

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -1,14 +1,44 @@
+import httpStatus from 'http-status';
 import supertest from 'supertest';
-import { cleanDb } from '../helpers';
+import { cleanDb, generateValidToken } from '../helpers';
+import { createHotelAndRoom, createTicketContext, createUser } from '../factories';
+import { createBooking } from '../factories/booking-factory';
+import describeRouteAuthentication from './common/route-authentication';
 import app, { init } from '@/app';
 
-const api = supertest(app);
+const server = supertest(app);
 
 beforeAll(async () => await init());
 beforeEach(async () => await cleanDb());
 
 describe('GET /booking', () => {
-  console.log('test here');
+  describeRouteAuthentication('/booking', 'get');
+
+  describe('when token is valid', () => {
+    it('should respond with status 404 if user has no booking', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+
+      const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });
+
+    it('should respond with status 200 and user booking', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+
+      const { room } = await createHotelAndRoom();
+      await createTicketContext(user);
+
+      const { id } = await createBooking({ userId: user.id, roomId: room.id });
+
+      const response = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.OK);
+      expect(response.body).toMatchObject({ id, Room: room });
+    });
+  });
 });
 
 describe('POST /booking', () => {
