f5fee4d95b27bca80b7825303556366cc6702fcb
samuel <shemlouk@gmail.com>
2023-04-26 16:39:36 -0300

    feat: add bookRoom function to booking controller

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -4,8 +4,18 @@ import { bookingService } from '@/services';
 
 export const getUserBooking = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   try {
-    const [userBooking] = await bookingService.getUserBookings(Number(req.userId));
+    const [userBooking] = await bookingService.getUserBookings(req.userId);
     return res.send(userBooking);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const bookRoom = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  const { roomId } = req.body;
+  try {
+    const booking = await bookingService.createBooking(req.userId, roomId);
+    return res.send(booking);
   } catch (error) {
     next(error);
   }
