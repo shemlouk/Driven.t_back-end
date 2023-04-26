00d718768893f4899c8d39735154b14ea72a73bf
samuel <shemlouk@gmail.com>
2023-04-26 16:38:02 -0300

    feat: add createBooking method to booking service

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,5 +1,6 @@
 import bookingRepository from '@/repositories/booking-repository';
-import { notFoundError } from '@/errors';
+import { notFoundError, roomUnavailableError } from '@/errors';
+import roomsRepository from '@/repositories/rooms-repository';
 
 const getUserBookings = async (userId: number) => {
   const bookings = await bookingRepository.findByUserIdWithCustomSelection(userId, { id: true, Room: true });
@@ -9,6 +10,18 @@ const getUserBookings = async (userId: number) => {
   return bookings;
 };
 
-export const bookingService = { getUserBookings };
+const createBooking = async (userId: number, roomId: number) => {
+  const room = await roomsRepository.findById(roomId);
+  if (!room) throw notFoundError();
+
+  const roomBookings = await bookingRepository.findByRoomId(roomId);
+  if (room.capacity === roomBookings.length) throw roomUnavailableError();
+
+  const { id } = await bookingRepository.create({ userId, roomId });
+
+  return { bookingId: id };
+};
+
+export const bookingService = { getUserBookings, createBooking };
 
 export default bookingService;
