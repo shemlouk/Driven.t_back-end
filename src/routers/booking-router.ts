5bd49fe316983a9e75987c2e473073ee3bfe6cc2
samuel <shemlouk@gmail.com>
2023-04-25 22:14:09 -0300

    feat: add /booking GET endpoint

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
+import { getUserBooking } from '@/controllers';
 
 const bookingRouter = Router();
 
-bookingRouter.all('/*', authenticateToken).get('/').post('/').put('/:bookingId');
+bookingRouter.all('/*', authenticateToken).get('/', getUserBooking).post('/').put('/:bookingId');
 
 export { bookingRouter };
