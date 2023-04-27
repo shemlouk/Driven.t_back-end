b0acc71c843ba3c7db309cc9d801b99a3332a617
samuel <shemlouk@gmail.com>
2023-04-27 00:25:42 -0300

    feat: add updateRoom to booking service

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,27 +1,37 @@
 import bookingRepository from '@/repositories/booking-repository';
-import { notFoundError, roomUnavailableError } from '@/errors';
+import { forbiddenError, notFoundError, roomUnavailableError } from '@/errors';
 import roomsRepository from '@/repositories/rooms-repository';
+
+const checkRoomAvailability = async (roomId: number) => {
+  const room = await roomsRepository.findById(roomId);
+  if (!room) throw notFoundError();
+  const roomReservations = await bookingRepository.findByRoomId(roomId);
+  if (room.capacity === roomReservations.length) throw roomUnavailableError();
+};
 
 const getUserBookings = async (userId: number) => {
   const bookings = await bookingRepository.findByUserIdWithCustomSelection(userId, { id: true, Room: true });
-
   if (!bookings.length) throw notFoundError();
-
   return bookings;
 };
 
 const createBooking = async (userId: number, roomId: number) => {
-  const room = await roomsRepository.findById(roomId);
-  if (!room) throw notFoundError();
-
-  const roomBookings = await bookingRepository.findByRoomId(roomId);
-  if (room.capacity === roomBookings.length) throw roomUnavailableError();
-
+  await checkRoomAvailability(roomId);
   const { id } = await bookingRepository.create({ userId, roomId });
-
   return { bookingId: id };
 };
 
-export const bookingService = { getUserBookings, createBooking };
+const updateRoom = async (bookingId: number, userId: number, roomId: number) => {
+  await checkRoomAvailability(roomId);
+
+  const booking = await bookingRepository.findById(bookingId);
+  if (!booking) throw notFoundError();
+  if (booking.userId !== userId) throw forbiddenError();
+
+  const { id } = await bookingRepository.updateRoomId(bookingId, roomId);
+  return { bookingId: id };
+};
+
+export const bookingService = { getUserBookings, createBooking, updateRoom };
 
 export default bookingService;
